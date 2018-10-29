37923395a9f165b0399ad3ab6828eb5cb0018077
Thekob <thekob.mx@gmail.com>
2018-10-29 15:41:51 +0700

    Two ViewControllers

---
+++ FunnyNumber29oct18/ArticleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArticleViewController.swift
+//  FunnyNumber29oct18
+//
+//  Created by kotchaphan on 29/10/2561 BE.
+//  Copyright © 2561 Thanapat.P. All rights reserved.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
