b916bae7c81daa200f86414ba24b4ce188662dc7
Thekob <thekob.mx@gmail.com>
2018-10-30 10:17:37 +0700

    Number Finish

---
+++ FunnyNumber29oct18/MyClass.swift
@@ -0,0 +1,32 @@
+//
+//  MyClass.swift
+//  FunnyNumber29oct18
+//
+//  Created by kotchaphan on 30/10/2561 BE.
+//  Copyright © 2561 Thanapat.P. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+    
+    
+    var myNumberInt: Int = 0
+    
+    
+    func decreaseNum() -> String {
+        myNumberInt -= 1
+        return String(myNumberInt)
+    }
+    
+    
+    func increase() -> String {
+        myNumberInt += 1
+        return String(myNumberInt)
+    }
+    
+    
+    
+} //My Class
+
+
--- FunnyNumber29oct18/ViewController.swift
+++ FunnyNumber29oct18/ViewController.swift
@@ -10,9 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    Explicit
+    var currentNumberInt: Int = 0
+    var myClass = MyClass()
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    
+    @IBAction func decreaseButton(_ sender: Any) {
+        
+//        Get Value from Label
+        currentNumberInt = findCurrentNum()
+        print("currentNumber ==> \(currentNumberInt)")
+        
+//        Assign Value to MyClass
+        myClass.myNumberInt = currentNumberInt
+        print("myNumberInt_myClass ==> \(myClass.myNumberInt)")
+        
+        numberLabel.text = myClass.decreaseNum()
+        
+    }
+    
+    
+    
+    @IBAction func increaseButton(_ sender: Any) {
+        
+        myClass.myNumberInt = findCurrentNum()
+        numberLabel.text = myClass.increase()
+        
+        
+    }
+    
+    
+    
+    @IBAction func homeButton(_ sender: Any) {
+        
+        numberLabel.text = "0"
+    }
+    
+    
+    
+    func findCurrentNum() -> Int {
+        let currentNumber: Int = Int(numberLabel.text!)!
+        return currentNumber
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
 
