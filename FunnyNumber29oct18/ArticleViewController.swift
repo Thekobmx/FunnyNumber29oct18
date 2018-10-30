204691a823ca3d8b56a37f7eb93f928750a17b99
Thekob <thekob.mx@gmail.com>
2018-10-30 10:53:14 +0700

    Decreae and Increase Finish

--- FunnyNumber29oct18/ArticleViewController.swift
+++ FunnyNumber29oct18/ArticleViewController.swift
@@ -10,21 +10,46 @@ import UIKit
 
 class ArticleViewController: UIViewController {
 
+    
+//    Explicit
+    var currentNumberInt: Int = 0
+    var myClass = MyClass()
+    
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    @IBAction func decreaseButton(_ sender: Any) {
+        
+        
+        myClass.myNumberInt = currentNumberInt
+        currentNumberInt = myClass.decreaseArticle()
+        numberLabel.text = myClass.articleNumberString[currentNumberInt]
+        
+    }
+    
+    
+    @IBAction func increaseButton(_ sender: Any) {
+        
+        myClass.myNumberInt = currentNumberInt
+        currentNumberInt = myClass.increaseArticle()
+        numberLabel.text = myClass.articleNumberString[currentNumberInt]
+        
+    }
+    
+    
+    @IBAction func homeButton(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        
+    } //ViewDidLoad
     
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+} //Main Class
--- FunnyNumber29oct18/MyClass.swift
+++ FunnyNumber29oct18/MyClass.swift
@@ -12,6 +12,34 @@ class MyClass {
     
     
     var myNumberInt: Int = 0
+    var articleNumberString = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",]
+    
+    
+    func increaseArticle() -> Int {
+        
+        if myNumberInt >= 10 {
+            myNumberInt = 0
+        } else{
+            myNumberInt += 1
+        }
+        
+        return myNumberInt
+    }
+    
+    
+    func decreaseArticle() -> Int {
+        
+        if myNumberInt <= 0 {
+            myNumberInt = 10
+        }
+        else
+        {
+            myNumberInt -= 1
+        }
+        
+        return myNumberInt
+    }
+    
     
     
     func decreaseNum() -> String {
